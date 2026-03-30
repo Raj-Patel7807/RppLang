0252199ba740487679648b9cffdd06345fe6abb6
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-03-31 02:43:56 +0530

    feat(runtime): add basic env

--- src/main/java/com/rpp/runtime/Environment.java
+++ src/main/java/com/rpp/runtime/Environment.java
@@ -1,4 +1,19 @@
 package com.rpp.runtime;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Environment {
+    private final Map<String, Integer> variables = new HashMap<>();
+
+    public void set(String name, int value) {
+        variables.put(name, value);
+    }
+
+    public int get(String name) {
+        if(!variables.containsKey(name)) {
+            throw new RuntimeException("Undefined variable: " + name);
+        }
+        return variables.get(name);
+    }
 }
