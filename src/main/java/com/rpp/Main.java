aaf4a8e022bdbd763efe7723ed44ab7f7d581cee
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-03-31 02:44:29 +0530

    feat: upd Main and make compiler working

--- src/main/java/com/rpp/Main.java
+++ src/main/java/com/rpp/Main.java
@@ -1,7 +1,34 @@
 package com.rpp;
 
-public class Main {
-    public static void main(String[] args) {
+import com.rpp.lexer.Lexer;
+import com.rpp.parser.Parser;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        if(args.length == 0) {
+            System.out.println("Usage: java com.rpp.Main <file.rpp>");
+            return;
+        }
+
+        String filePath = args[0];
+
+        if(!filePath.endsWith(".rpp")) {
+            System.out.println("Error: Only .rpp files allowed");
+            return;
+        }
+
+        String code = new String(
+                Files.readAllBytes(Paths.get(filePath))
+        );
+
+        Lexer lexer = new Lexer(code);
+
+        Parser parser = new Parser(lexer.tokenize());
+
+        parser.parse();
     }
 }
