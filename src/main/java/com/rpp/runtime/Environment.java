8c010c921fc6f46fdd7b10dfd37d3c405a6dc746
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-03-31 23:57:08 +0530

    feat(runtime): make env more flexible

--- src/main/java/com/rpp/runtime/Environment.java
+++ src/main/java/com/rpp/runtime/Environment.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
-    private final Map<String, Integer> variables = new HashMap<>();
+    private final Map<String, Object> variables = new HashMap<>();
 
-    public void set(String name, int value) {
+    public void set(String name, Object value) {
         variables.put(name, value);
     }
 
-    public int get(String name) {
+    public Object get(String name) {
         if(!variables.containsKey(name)) {
             throw new RuntimeException("Undefined variable: " + name);
         }
